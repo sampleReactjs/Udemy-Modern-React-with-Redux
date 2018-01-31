48fa0ad17bbd9abfad54357b42334d7903ae9f01
srh <s.hosseyni2013@gmail.com>
2018-01-31 16:46:44 +0330

    inp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component{
+
+  render() {
+    return (
+      <Fragment>
+        <div>Test</div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
